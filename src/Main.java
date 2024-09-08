044e795f638537b3db4820332ed72b2b115a5a6b
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2024-09-08 17:53:46 +0530

    problamatic code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import ProblamaticCode.Employee;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        e.printPerformance();
+    }
+}
---
+++ src/ProblamaticCode/Employee.java
@@ -0,0 +1,22 @@
+package ProblamaticCode;
+
+// This class also violates Single Responsibility Principle
+public class Employee {
+    private int id;
+
+    public int getId() {
+        return 0;
+    }
+
+    public String fetchBioData() {
+        return "Bio Data";
+    }
+
+    public double calculateSalary() {
+        return 0;
+    }
+
+    public void printPerformance() {
+        System.out.println("Performance");
+    }
+}
