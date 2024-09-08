ce6d7ba277919dfd3bd1dd062e1ad26557b820fc
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2024-09-08 18:28:38 +0530

    example 3 problamatic code

---
+++ src/Example3/ProblamaticCode/UserManager.java
@@ -0,0 +1,15 @@
+package Example3.ProblamaticCode;
+
+public class UserManager {
+    public void createUser(String username) {
+        System.out.println("User Created");
+    }
+
+    public void deleteUser(String username) {
+        System.out.println("User Deleted");
+    }
+
+    public void logUserActivity(String username) {
+        System.out.println("No Log Activity");
+    }
+}
